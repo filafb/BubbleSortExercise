e91001ba02efa4e88fe675a8e019a651f4d3cb1e
wildapt01 <pb5191@gmail.com>
2018-06-11 14:35:00 -0400

    working

--- bubblesort.js
+++ bubblesort.js
@@ -1,34 +1,49 @@
-let arr = [3, 2, 1, 4];
 
-function comparing(left, right) {
-  let max = left > right ? left : right;
-  return max;
-}
+// let arr = [3, 2, 1, 4];
+// function comparing(left, right) {
+//   let max = left > right ? left : right;
+//   return max;
+// }
 
-function bubbleSort(arr) {
-  let result = [];
-  let swapCounter = 0;
-  if(!arr.length) return arr;
+// function bubbleSort(arr) {
+//   let result = [];
+//   let swapCounter = 0;
+//   if(!arr.length) return arr;
 
-  for (let i = 0; i < arr.length -1; i++){
-      // let tempArr = arr.splice(i, 2) // [3,2] & arr [1, 4]
-      // //  arr = [3, 2, 1, 4]
-      // if(arr[0] > arr[1]) {
-      //    tempArr = tempArr.reverse(); // [2,3]
-      // }
-      // arr = tempArr.concat(arr); // arr = [2,3,1,4] use splice to get values in the middle
-    if(arr[i] > arr[i+1]){
-      let greater = arr[i];
-      let lower = arr[i+1];
-      arr[i] = lower;
-      arr[i+1] = greater;
-      swapCounter++
+//   for (let i = 0; i < arr.length -1; i++){
+//       // let tempArr = arr.splice(i, 2) // [3,2] & arr [1, 4]
+//       // //  arr = [3, 2, 1, 4]
+//       // if(arr[0] > arr[1]) {
+//       //    tempArr = tempArr.reverse(); // [2,3]
+//       // }
+//       // arr = tempArr.concat(arr); // arr = [2,3,1,4] use splice to get values in the middle
+//     if(arr[i] > arr[i+1]){
+//       let greater = arr[i];
+//       let lower = arr[i+1];
+//       arr[i] = lower;
+//       arr[i+1] = greater;
+//       swapCounter++
+//       }
+//     }
+//     return bubbleSort(arr.slice(0,arr.length-1))
+//   }
+
+  function bubbleSort(arr) {
+    let interations = arr.length
+    while(interations){
+
+    for (let i = 0; i < interations -1; i++){
+      if(arr[i] > arr[i+1]){
+        let greater = arr[i];
+        let lower = arr[i+1];
+        arr[i] = lower;
+        arr[i+1] = greater;
+        }
       }
+      interations--
     }
-    return bubbleSort(arr.slice(0,arr.length-1))
-  }
-
-
+      return  arr
+    }
 
 
 
