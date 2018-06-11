2632df101150ee92b04fe93f17bb2fdc53a695c5
Filadelfo Braz <filadelfo.braz@gmail.com>
2018-06-11 15:31:44 -0400

    moving the greater to the end

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,16 +1,8 @@
-
-
 describe('Bubble Sort', function(){
-
-  beforeEach(() => {
-    let  swapCounter = 0;
-    let  comparisonCounter = 0;
-  });
-
-
   it('handles an empty array', function(){
     expect( bubbleSort([]) ).toEqual( [] );
   });
+
 
   it('handles an array with one element',function(){
     expect ( bubbleSort([14])).toEqual( [14] );
@@ -33,5 +25,4 @@ describe('Bubble Sort', function(){
   it('handles an array mixing numbers and letters',function(){
     expect ( bubbleSort([6, 5, 'Patrick', 4, 'Fila', 2, 1])).toEqual( [1, 2, 4, 5, 6, 'Fila', 'Patrick'] );
   });
-
 });
