f3014adb769ac9e55d6e4f66aab0ce4258b9801e
wildapt01 <pb5191@gmail.com>
2018-06-11 16:52:17 -0400

    mergeSort not functionning

--- mergesort.js
+++ mergesort.js
@@ -7,18 +7,11 @@ function split(arr){
   return [leftArr, rightArr];
 }
 
-function mergeSort (arr) {
-
-
-
-}
-
 function merge(arr){
   // [[1,2,4], [3,5,6]] --> [1,2,3,4,5,6]
   let arr1 = arr[0];
   let arr2 = arr[1];
   let finalArr = [];
-  // let greaterLength = arr1.length > arr2.length ? arr1.length : arr2.length;
   while(arr1.length || arr2.length){
     if(arr1[0] < arr2[0]){
       finalArr.push(arr1.shift());
@@ -29,3 +22,23 @@ function merge(arr){
   }
   return finalArr
 }
+
+function mergeSort (arr) {
+  if (arr.length < 2) return arr;
+
+  const splitArr = mergeSort(split(arr));
+
+  return merge(splitArr);
+}
+
+// Array -> split in 2
+//   2 arrays, each split in 2
+//     again
+//       again
+//     => arr = [[3], [2], [1], [5], [7], [6], [4]]
+
+//     merge arr[0] arr[1]
+//       => arr [[2, 3], [1, 5], [6, 7], [4]]
+//       merge
+//         => arr 1,2,3,5  4,6,7
+//           => arr 1234567  (return)
