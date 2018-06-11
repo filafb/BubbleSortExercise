d7c9b7fb9fabccd770624b5454a0ae3909aa6423
wildapt01 <pb5191@gmail.com>
2018-06-11 15:27:46 -0400

    split done

---
+++ mergesort.js
@@ -0,0 +1,14 @@
+// mergeSort()
+
+function split(arr){
+  const mid = Math.floor(arr.length / 2);
+  const leftArr = arr.slice(0, mid);
+  const rightArr = arr.slice(mid);
+  return [leftArr, rightArr];
+}
+
+function mergeSort (arr) {
+
+
+
+}
