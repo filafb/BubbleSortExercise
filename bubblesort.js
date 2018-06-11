379064bc6f010e54aedc1eb4f08084191f37ef40
Filadelfo Braz <filadelfo.braz@gmail.com>
2018-06-11 14:19:57 -0400

    moving the greater to the end

--- bubblesort.js
+++ bubblesort.js
@@ -8,21 +8,24 @@ function comparing(left, right) {
 function bubbleSort(arr) {
   let result = [];
   let swapCounter = 0;
+  if(!arr.length) return arr;
 
-  while (result.length <= arr.length) {
-    for (let i = 0; i < arr.length - 1; i++){
+  for (let i = 0; i < arr.length -1; i++){
       // let tempArr = arr.splice(i, 2) // [3,2] & arr [1, 4]
       // //  arr = [3, 2, 1, 4]
       // if(arr[0] > arr[1]) {
       //    tempArr = tempArr.reverse(); // [2,3]
       // }
       // arr = tempArr.concat(arr); // arr = [2,3,1,4] use splice to get values in the middle
-
-
-
-
+    if(arr[i] > arr[i+1]){
+      let greater = arr[i];
+      let lower = arr[i+1];
+      arr[i] = lower;
+      arr[i+1] = greater;
+      swapCounter++
+      }
     }
-
+    return bubbleSort(arr.slice(0,arr.length-1))
   }
 
 
